cd0112fd7bde8308ffebb7f0911fd8a7614ada10
Minh Duy <23110143@student.hcmus.edu.vn>
2025-11-19 23:28:11 +0700

    Update: Processor: Xây dựng lại bộ lọc giá trị EDA: Xây dựng thêm các đồ thị, và lựa chọn môn học Run Pepline: Nothing

--- run_pipeline.py
+++ run_pipeline.py
@@ -9,55 +9,55 @@ import time
 import pandas as pd
 import numpy as np
 
-def main():
-    start = time.perf_counter()
-    # Khởi tạo processor và gán loader (setter sẽ ok)
-    ROOT_DIR = Path(__file__).resolve().parent           # thư mục PythonProject
-    CLEAN_DIR = ROOT_DIR / "Data" / "Clean_data_2023-2025"
-    CLEAN_DIR.mkdir(parents=True, exist_ok=True)         # tạo nếu chưa tồn tại
+# def main():
+#     # start = time.perf_counter()
+#     # # Khởi tạo processor và gán loader (setter sẽ ok)
+#     # ROOT_DIR = Path(__file__).resolve().parent           # thư mục PythonProject
+#     # CLEAN_DIR = ROOT_DIR / "Data" / "Clean_data_2023-2025"
+#     # CLEAN_DIR.mkdir(parents=True, exist_ok=True)         # tạo nếu chưa tồn tại
 
-    proc = DataProcessor()
+#     # proc = DataProcessor()
 
-    # Gọi pipeline xử lý rồi lấy dữ liệu
-    proc.process_all()
-    df = proc.get_processed_data()
+#     # # Gọi pipeline xử lý rồi lấy dữ liệu
+#     # proc.process_all()
+#     # df = proc.get_processed_data()
 
-    # Phân tích dữ liệu
-    analysis = Analysis(proc)
+#     # # Phân tích dữ liệu
+#     # analysis = Analysis(proc)
 
-    # ================== MÔN HỌC ==================
-    # map tên hiển thị -> tên cột trong DataFrame
-    subject_map = {
-        "Toan":  "toan",
-        "Van":   "ngu_van",
-        "Anh":   "ngoai_ngu",
-        "Ly":    "vat_li",
-        "Hoa":   "hoa_hoc",
-        "Sinh":  "sinh_hoc",
-        "Su":    "lich_su",
-        "Dia":   "dia_li",
-        "GDCD":  "gdcd",
-    }
+#     # # ================== MÔN HỌC ==================
+#     # # map tên hiển thị -> tên cột trong DataFrame
+#     # subject_map = {
+#     #     "Toan":  "toan",
+#     #     "Van":   "ngu_van",
+#     #     "Anh":   "ngoai_ngu",
+#     #     "Ly":    "vat_li",
+#     #     "Hoa":   "hoa_hoc",
+#     #     "Sinh":  "sinh_hoc",
+#     #     "Su":    "lich_su",
+#     #     "Dia":   "dia_li",
+#     #     "GDCD":  "gdcd",
+#     # }
 
-    for short_name, col_name in subject_map.items():
-        # dùng phân phối điểm theo môn (theo từng năm)
-        df_subject = analysis.get_arregate_by_exam_subsections(col_name)
+#     # for short_name, col_name in subject_map.items():
+#     #     # dùng phân phối điểm theo môn (theo từng năm)
+#     #     df_subject = analysis.get_arregate_by_exam_subsections(col_name)
     
-        filename = CLEAN_DIR / f"Export_Analysis_Subject_{short_name}_2023-2025.csv"
-        df_subject.to_csv(filename, index=False)
+#     #     filename = CLEAN_DIR / f"Export_Analysis_Subject_{short_name}_2023-2025.csv"
+#     #     df_subject.to_csv(filename, index=False)
 
-    # ================== KHỐI THI ==================
-    blocks = ["A", "B", "C", "D"]
+#     # # ================== KHỐI THI ==================
+#     # blocks = ["A", "B", "C", "D"]
 
-    for block in blocks:
-        # phân phối tổng điểm theo khối & năm
-        df_block = analysis.analyze_scores_by_exam_block(block)
+#     # for block in blocks:
+#     #     # phân phối tổng điểm theo khối & năm
+#     #     df_block = analysis.analyze_scores_by_exam_block(block)
     
-        filename = CLEAN_DIR / f"Export_Analysis_Block_{block}_2023-2025.csv"
-        df_block.to_csv(filename, index=False)
+#     #     filename = CLEAN_DIR / f"Export_Analysis_Block_{block}_2023-2025.csv"
+#     #     df_block.to_csv(filename, index=False)
 
-    end = time.perf_counter()
-    print(f"Elapsed: {end - start:.6f} s")
+#     # end = time.perf_counter()
+#     # print(f"Elapsed: {end - start:.6f} s")
 
 if __name__ == "__main__":
     main()
